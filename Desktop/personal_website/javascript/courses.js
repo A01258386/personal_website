287d6f5e44c4b6b821902a5cb8dc79dede4a857e
Gokce <gokmengokces@gmail.com>
2021-03-31 18:34:52 -0700

    code and name data types changed

--- Desktop/personal_website/javascript/courses.js
+++ Desktop/personal_website/javascript/courses.js
@@ -1,6 +1,14 @@
-const courseList= [{ 'code': "ACIT 1620" ,  "name": " Web Fundamental Technologies"},{ 'code': "ACIT 1515" ,  "name": " Scripting for IT"},{ 'code': "ACIT 1630" ,  "name": "Database Systems"},]
+const courseList= [{ code: "1620" ,  name: " Web Fundamental Technologies"},{ code: "1515" ,  name: " Scripting for IT"},{ code: "1630" ,  name: "Database Systems"},]
 let user;
 do{
    user= prompt("your 4 digit number");
 } while(!isNaN(user) || user.length != 4); 
 user= prompt("your 4 digit number");
+
+for(let code of courseList)
+{
+    if(stu.program === "CIT" && stu.GPA > 3)
+    {
+        counter ++;
+    }
+}
