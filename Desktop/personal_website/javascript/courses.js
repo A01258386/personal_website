6a5bef3abb38890859fc10c45cd2758e400f65f2
Gokce <gokmengokces@gmail.com>
2021-03-31 18:30:52 -0700

    user prompt loop is created

--- Desktop/personal_website/javascript/courses.js
+++ Desktop/personal_website/javascript/courses.js
@@ -1,1 +1,6 @@
 const courseList= [{ 'code': "ACIT 1620" ,  "name": " Web Fundamental Technologies"},{ 'code': "ACIT 1515" ,  "name": " Scripting for IT"},{ 'code': "ACIT 1630" ,  "name": "Database Systems"},]
+let user;
+do{
+   user= prompt("your 4 digit number");
+} while(!isNaN(user) || user.length != 4); 
+user= prompt("your 4 digit number");
